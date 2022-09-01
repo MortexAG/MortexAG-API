7e0035d26193723a69fd6c94a0c93f6eabe9410a
MortexAG <107272049+MortexAG@users.noreply.github.com>
2022-09-01 11:10:29 +0000

    made the real api

--- index.js
+++ index.js
@@ -7,25 +7,40 @@ app.listen(
 
 )
 
-app.get('/tshirt', (req, res) => {
+app.get('/github', (req, res) => {
   res.status(200).send({
-    tshirt: 'hola shirto',
-    size: 'smol'
+    my_account:"https://github.com/MortexAG",
+    my_site: 'https://github.com/MortexAG/mortexagsite',
+    currency_converter: 'https://github.com/MortexAG/Currency-Converter',
+    file_encryption_tool:'https://github.com/MortexAG/Reactive-File-encryption-decryption',
+    telegram_bots: {
+      the_repo:'https://github.com/MortexAG/Telegram-Bots',
+      clock_bot:'https://github.com/MortexAG/Telegram-Bots/tree/main/Clock%20Bot',
+      daily_message_bot:'https://github.com/MortexAG/Telegram-Bots/tree/main/Daily%20Message%20Bot',
+      document_converter_bot:'https://github.com/MortexAG/Telegram-Bots/tree/main/Document-Converter',
+      weather_bot:'https://github.com/MortexAG/Telegram-Bots/tree/main/Weather%20Bot',
+      file_encryption_tool_bot:'https://github.com/MortexAG/Reactive-File-encryption-decryption',
+      youtube_audio_downloader_bot:'https://github.com/MortexAG/Telegram-Bots/tree/main/Youtube%20Video%20To%20Audio%20Downloader',
+    }
   })
 });
 
-app.get('/mortdata', (req, res) => {
+app.get('/replit', (req, res) => {
   res.status(200).send({
-    mort: 'is fine',
-    ahmed: 'is good',
-    how_you: 'pretty fine',
-    le_cheese: {
-      mortad: "not good",
-      gorga: 'acceptaple'
+    my_account:'https://replit.com/@MortexAG',
+    discord_bots:{ calm_gamers_music_bot:'https://replit.com/@MortexAG/CalmGamersBot',
+calm_gamers_bot_commands:'https://replit.com/@MortexAG/CalmGamersBotCommands',
+      gamers_music_bot_v1:'https://replit.com/@MortexAG/GamersMusicBot',
+      gamers_music_bot_v2:'https://replit.com/@MortexAG/GamersMusicBot2',
+
+gamers_music_bot_commands:'https://replit.com/@MortexAG/GamersMusicBotCommand'
+                  
     }
-  });
+    })
 });
 
+/* Some Code To Make A Post Request (will be added later) */
+/*
 app.post('/tshirt/:id', (req, res) => {
   const { id } = req.params;
   const { logo } = req.body;
@@ -40,3 +55,5 @@ app.post('/tshirt/:id', (req, res) => {
     tshirt: `shirto with your logo  "${logo}" and ID of ${id} `,
   });
 });
+
+*/
